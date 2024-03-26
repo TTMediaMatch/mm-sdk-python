d3cc76e92c2b72c320b4722cf90544298b76ae89
shengyang wang <shengyang.wang@bytedance.com>
2024-03-26 15:20:15 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ttmediamatch",
+    version="0.1.0",
+    keywords=("pip", "ttmediamatch", "mm-sdk-python"),
+    description="The Python SDK for Mediamatch",
+    license="MIT Licence",
+
+    url="https://github.com/TTMediaMatch/mm-sdk-python",
+    author="TTMediamatch SDK",
+    author_email="shengyang.wang@bytedance.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["requests"]
+)
