14245a50164189052585f30a1783f149ea079f9a
shengyang.wang <shengyang.wang@bytedance.com>
2024-03-28 10:40:44 -0700

    bug fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ttmediamatch",
-    version="0.1.1",
+    version="0.1.2",
     keywords=("pip", "ttmediamatch", "mm-sdk-python"),
     description="The Python SDK for Mediamatch",
     license="MIT Licence",
