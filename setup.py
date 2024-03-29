e88197c736da3065fdb192c1416946ae7532bd6b
shengyang.wang <shengyang.wang@bytedance.com>
2024-03-29 11:18:44 -0700

    update example

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ttmediamatch",
-    version="0.1.5",
+    version="0.1.51",
     keywords=("pip", "ttmediamatch", "mm-sdk-python"),
     description="The Python SDK for Mediamatch",
     license="MIT Licence",
