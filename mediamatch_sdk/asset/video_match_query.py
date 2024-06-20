6b77c9fdd1223160a20543ced671da24e0cb7434
houxiangrui <houxiangrui.hou@bytedance.com>
2024-06-20 11:58:09 +0800

    fix

--- mediamatch_sdk/asset/video_match_query.py
+++ mediamatch_sdk/asset/video_match_query.py
@@ -8,8 +8,11 @@ class VideoMatchQuery(BaseClient):
     def __init__(self, access_token):
         super().__init__(access_token)  # Initialize the BaseClient with the access token
 
-    def get_video_match(self, asset_id,start_time,end_time,page,pageSize):
-        response = self._get(path=f"openapi/asset/v1/video/assets/{asset_id}/matchinfo?startTime={start_time}&endTime={end_time}&page={page}&pageSize={pageSize}")
+    def get_video_match(self, asset_id,start_time,end_time,page=None,pageSize=None):
+        if (page == None or pageSize == None):
+            response = self._get(path=f"openapi/asset/v1/video/assets/{asset_id}/matchinfo?startTime={start_time}&endTime={end_time}")
+        else:
+            response = self._get(path=f"openapi/asset/v1/video/assets/{asset_id}/matchinfo?startTime={start_time}&endTime={end_time}&page={page}&pageSize={pageSize}")
         if response.status_code == 200:
             return response.json()  # Assuming this returns the stream_info
         else:
